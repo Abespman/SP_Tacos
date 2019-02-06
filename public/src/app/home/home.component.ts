5c45057257006269b34f14c727f4fbb04f23cb4a
Rob Knox <r.knox@mac.com>
2019-02-06 14:03:05 -0800

    Added routing, imported httpClient, created observables for the homepage

--- public/src/app/home/home.component.ts
+++ public/src/app/home/home.component.ts
@@ -8,10 +8,28 @@ import { Router, ActivatedRoute } from '@angular/router';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
-  constructor(private _http: HttpService, private _router: Router, private _route: ActivatedRoute) { }
+  currentUser = {email: ''};
+  items = [];
+  constructor(private _httpService: HttpService, private _router: Router, private _route: ActivatedRoute) { }
 
   ngOnInit() {
+    var tempObservable = this._httpService.getCurrentUser();
+    tempObservable.subscribe((data: any) => {
+      if (data.error) {
+        this._router.navigate(['/']);
+      } else {
+        this.currentUser = data;
+      }
+    })
+
+    var tempObservable2 = this._httpService.getAllItems();
+    tempObservable2.subscribe((data: any) => {
+      if (data.error) {
+        this._router.navigate(['/']);
+      } else {
+        this.items = data;
+      }
+    })
   }
 
 }
