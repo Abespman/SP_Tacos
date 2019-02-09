c9f27745e1c8ae65b3f8ddf4066fd3a8797fb367
Abraham Cruz <cruz_abraham@icloud.com>
2019-02-09 00:13:35 -0800

    changed the server.js file

--- server.js
+++ server.js
@@ -1,1 +1,22 @@
-hello
+const express = require("express");
+const app = express();
+const mongoose = require ("mongoose");
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.json());
+app.use(express.static(__dirname + "/publc/dist/public"));
+
+mongoose.connect("mongodb://localhost/ SP_Tacos");
+
+require("./server/config/mongoose.js");
+require("./server/models/model.js");
+require("./server/config/routes.js")(app);
+app.all("*", (req, res, next) =>{
+    res.sendFile(path.resolve("./public/dist/public/index.html"))
+});
+
+app.listen(8000, function() {
+    console.log("Listening on port 8000.");
+});
+
+
