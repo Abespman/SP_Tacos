d7144d1c2d97a93d47b210c30e879de7fbd5656b
Abraham Cruz <cruz_abraham@icloud.com>
2019-02-10 16:48:18 -0800

    created the rest of the controllers

--- server/controllers/controllers.js
+++ server/controllers/controllers.js
@@ -6,17 +6,32 @@ const Model = mongoose.model("Product");
 module.exports = {
 
     index: function(req, res){
-        Model.find({},function(err, model) {
+        Product.find({},function(err, model) {
             if(err) {
                 console.log("Returned an error.");
-                res.jason(model);
-
+                res.json({message: "Error"}, err);
+            }else{
+                console.log("Sucess");
+                res.json(model);
             }
         })
     },
     showOne: function(req, res){
-        Model.findOne({_id: req.params.id}, function(err, model){
+        Product.findOne({_id: req.params.id}, function(err, model){
         });
     },
-    
+    delete: function(req, res){
+        Product.remove({_id: req.params.id}, function(err, model){
+        })
+    },
+    update: function(req, res){
+        Product.findOne({_id: req.params.id}, function(err, model){
+            product.title = req.body.title,
+            product.price = req.body.price,
+            product.description = req.body.description,
+            product.image = req.body.image,
+            product.save(function(err){
+            });
+        });
+    },
 }
