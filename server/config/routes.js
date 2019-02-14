5662b99a500570d31e0cfcae068ee93d98ed1f68
Abraham Cruz <cruz_abraham@icloud.com>
2019-02-13 21:35:54 -0800

    commented out data

--- server/config/routes.js
+++ server/config/routes.js
@@ -3,12 +3,12 @@ const path = require ("path");
 
 module.exports = function(app){
     app.get("/shop", controllers.index);
-    app.post("/api/login", controllers.login);
-    app.post("/api/register", controllers.register);
-    app.get("/api/details/:id", controllers.details);
-    app.delete("/api/delete/:id", controllers.delete);
-    app.put("/api/update", controllers.update);
-    app.post("/api/new", controllers.new);
+    // app.post("/api/login", controllers.login);
+    // app.post("/api/register", controllers.register);
+    // app.get("/api/details/:id", controllers.details);
+    // app.delete("/api/delete/:id", controllers.delete);
+    // app.put("/api/update", controllers.update);
+    // app.post("/api/new", controllers.new);
     
     app.all('*', (req, res, next) => {
         res.sendFile(path.resolve('./public/dist/public/index.html'));
--- server/controllers/controllers.js
+++ server/controllers/controllers.js
@@ -17,88 +17,88 @@ module.exports = {
             }
         })
     },
-    details: function(req, res){
-        Product.findOne({_id: req.params.id}, function(err, model){
-        });
-    },
-    delete: function(req, res){
-        Product.remove({_id: req.params.id}, function(err, model){
-        })
-    },
-    update: function(req, res){
-        Product.findOne({_id: req.params.id}, function(err, model){
-            product.title = req.body.title,
-            product.price = req.body.price,
-            product.description = req.body.description,
-            product.image = req.body.image,
-            product.save(function(err){
-                if(err){
-                    console.log(err);
-                    res.jason(err);
-                }
-            });
-        });
-    },
-    register: function(req, res) {
-        bcrypt.hash(req.body.password, 8, function(err, hash) {
-            if (err) {
-                console.log('error', err);
-                res.json(err);
-            } else {
-                req.body.password = hash;
-                var newUser = new User(req.body);
-                newUser.save(function(err) {
-                    if(err) {
-                        console.log('validation errors', err);
-                    } else {
-                        console.log('registered a new user!');
-                        req.session.userId = newUser._id;
-                        res.json(newUser);
-                    }
-                })
-            }
-        })
-    },
-    login: function(req, res) {
-        User.findOne({email: req.body.email}, function(err, foundUser) {
-            if(foundUser) {
-                bcrypt.compare(req.body.password, foundUser.password, function(err, result) {
-                    if (err) {
-                        console.log("passwords didn't match");
-                        res.json({error: 'Invalid Login'});
-                    } else {
-                       console.log('passwords match');
-                       req.session.userId = foundUser._id;
-                       res.json({message: "logged in!"});
-                    }
-                })
-            }
-        })
-    },
-    delete: function(req, res) {
-        console.log('Hit delete');
-        Product.remove({_id: req.params.id}, function(err, product) {
-            if (err) {
-                console.log(err);
-                res.json(err);
-            } else {
-                console.log("Deleted the ");
-                res.json(restaurant);
-            }
-        })
-    },
-    new: function(req, res){
-        console.log("hit create");
-        var newProduct = new Product(req.body);
-        newProduct.save(function(err){
-            if(err){
-                console.log("returned and error");
-                res.json(err);
-            }else{
-                console.log("created a product");
-                res.jason(newItem);
-            }
-        })
-    }
+    // details: function(req, res){
+    //     Product.findOne({_id: req.params.id}, function(err, model){
+    //     });
+    // },
+    // delete: function(req, res){
+    //     Product.remove({_id: req.params.id}, function(err, model){
+    //     })
+    // },
+    // update: function(req, res){
+    //     Product.findOne({_id: req.params.id}, function(err, model){
+    //         product.title = req.body.title,
+    //         product.price = req.body.price,
+    //         product.description = req.body.description,
+    //         product.image = req.body.image,
+    //         product.save(function(err){
+    //             if(err){
+    //                 console.log(err);
+    //                 res.jason(err);
+    //             }
+    //         });
+    //     });
+    // },
+    // register: function(req, res) {
+    //     bcrypt.hash(req.body.password, 8, function(err, hash) {
+    //         if (err) {
+    //             console.log('error', err);
+    //             res.json(err);
+    //         } else {
+    //             req.body.password = hash;
+    //             var newUser = new User(req.body);
+    //             newUser.save(function(err) {
+    //                 if(err) {
+    //                     console.log('validation errors', err);
+    //                 } else {
+    //                     console.log('registered a new user!');
+    //                     req.session.userId = newUser._id;
+    //                     res.json(newUser);
+    //                 }
+    //             })
+    //         }
+    //     })
+    // },
+    // login: function(req, res) {
+    //     User.findOne({email: req.body.email}, function(err, foundUser) {
+    //         if(foundUser) {
+    //             bcrypt.compare(req.body.password, foundUser.password, function(err, result) {
+    //                 if (err) {
+    //                     console.log("passwords didn't match");
+    //                     res.json({error: 'Invalid Login'});
+    //                 } else {
+    //                    console.log('passwords match');
+    //                    req.session.userId = foundUser._id;
+    //                    res.json({message: "logged in!"});
+    //                 }
+    //             })
+    //         }
+    //     })
+    // },
+    // delete: function(req, res) {
+    //     console.log('Hit delete');
+    //     Product.remove({_id: req.params.id}, function(err, product) {
+    //         if (err) {
+    //             console.log(err);
+    //             res.json(err);
+    //         } else {
+    //             console.log("Deleted the ");
+    //             res.json(restaurant);
+    //         }
+    //     })
+    // },
+    // new: function(req, res){
+    //     console.log("hit create");
+    //     var newProduct = new Product(req.body);
+    //     newProduct.save(function(err){
+    //         if(err){
+    //             console.log("returned and error");
+    //             res.json(err);
+    //         }else{
+    //             console.log("created a product");
+    //             res.jason(newItem);
+    //         }
+    //     })
+    // }
 
 }
